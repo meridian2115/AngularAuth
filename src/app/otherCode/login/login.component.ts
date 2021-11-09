58b5c3fd5c5293256eaa177ed79ba2ac337c1ee1
danilov.fedor <danilov.fedor@otr.ru>
2021-11-09 22:01:43 +0300

    #3 unUsedCode

--- src/app/otherCode/login/login.component.ts
+++ src/app/otherCode/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { AuthLoginInfo } from '../view/login-block/blocks/login-block/login-info';
+import { AuthLoginInfo } from '../../view/login-block/blocks/login-block/login-info';
 import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
@@ -17,23 +17,23 @@ export class LoginComponent implements OnInit {
   roles: string[] = [];
   username: string;
   private loginInfo: AuthLoginInfo;
- 
+
   constructor(private authService: AuthService, private tokenStorage: TokenStorageService) {}
- 
+
   ngOnInit() {
     if (this.tokenStorage.getToken()) {
       this.isLoggedIn = true;
       this.roles = this.tokenStorage.getAuthorities();
     }
   }
- 
+
   onSubmit() {
     console.log(this.form);
- 
+
     this.loginInfo = new AuthLoginInfo(
       this.form.username,
       this.form.password);
- 
+
     this.authService.attemptAuth(this.loginInfo).subscribe(
       data => {
         console.log(data);
@@ -43,7 +43,7 @@ export class LoginComponent implements OnInit {
         this.tokenStorage.saveToken(data.token);
         this.tokenStorage.saveUsername(data.username);
         this.tokenStorage.saveAuthorities(data.roles);
- 
+
         this.isLoginFailed = false;
         this.isLoggedIn = true;
         this.roles = this.tokenStorage.getAuthorities();
@@ -57,7 +57,7 @@ export class LoginComponent implements OnInit {
     );
     this.username = this.loginInfo.username;
   }
- 
+
   reloadPage() {
     window.location.reload();
   }
