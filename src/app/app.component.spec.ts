e96f5f471bf2c3158491d37b3cb01aa441eba7e3
danilov.fedor <danilov.fedor@otr.ru>
2021-11-11 20:31:51 +0300

    Удаление ненужного кода otherCode

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'conditer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('conditer');
-  });
+  // it(`should have as title 'conditer'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('conditer');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
